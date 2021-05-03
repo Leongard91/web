a5a929a7d6c8a96b45ec93595c2d935ed2b4d48f
Leongard91 <shureg91@gmail.com>
2021-05-03 19:04:28 +0300

    gg

--- SQL/commerce/auctions/admin.py
+++ SQL/commerce/auctions/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 
-<<<<<<< HEAD
-from .models import User, Listings, Category
-=======
 from .models import User, Listings, Category, Comments, Bids
->>>>>>> 033d65887f968d7b50687a8ec462b81c16c05a9a
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
@@ -12,10 +8,6 @@ class UserAdmin(admin.ModelAdmin):
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Category)
-<<<<<<< HEAD
-admin.site.register(Listings)
-=======
 admin.site.register(Listings)
 admin.site.register(Comments)
 admin.site.register(Bids)
->>>>>>> 033d65887f968d7b50687a8ec462b81c16c05a9a
