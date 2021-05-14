17d183b6e5d52ba27b9d2cd447e43446e581c1cd
Leongard91 <shureg91@gmail.com>
2021-05-14 23:33:14 +0300

    at_home_14051

--- Testing/project4/network/static/network/index.js
+++ Testing/project4/network/static/network/index.js
@@ -1,26 +1,30 @@
 document.addEventListener('click', event => {
-    if (event.target.className === "btn btn-link") {
-// !!!!!!!!11111
-        class App extends React.Component {
+    if (event.target.id === "edit") {
+        const previous = event.target.parentElement.innerHTML;
+        const previous_text = document.querySelector(`#post_text_${event.target.value}`).innerHTML;
+        
+        class App extends React.Component  {
+
             render() {
                 return (
-                    <div class="form_area" style="margin-top: 10px;">
-                        <form>
-                            <textarea name="post" cols="40" rows="10" class="post_form" maxlength="255" id="id_post">
-                                document.querySelector() 
-                            </textarea>
-                            <input type="submit" class="btn btn-primary" id="post_button" value="Save">
-                        </form>
+                    <div className="form_area">
+                        <br/>
+                        <textarea name="post" cols="40" rows="10" className="post_form" maxLength="255" id="id_post">
+                            {this.props.previous_text}
+                        </textarea>
+                        <input type="submit" className="btn btn-primary" id="post_button" value="Save" />
+                        <button type="button" className="btn btn-link" onClick={this.back} id='back'>Back</button>
                     </div>
                 );
-            }
+            };
+
         }
 
-        ReactDOM.render(<App />, document.querySelector('.post_area'));
+        ReactDOM.render(<App previous_text={previous_text} />, document.querySelector(`#post_${event.target.value}`));
+        
+        document.querySelector("#back").addEventListener('click', () => {
+            document.querySelector(`#post_${event.target.value}`).innerHTML = previous;
+        });
     }
+    return false;
 });
-
-
-
-
-
